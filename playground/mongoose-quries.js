f4d31218272580d5473bd04d84814a2d60bad7b8
unknown <engr.ab20@gmail.com>
2019-04-25 15:28:40 +0500

    quring mongoose

---
+++ playground/mongoose-quries.js
@@ -0,0 +1,31 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {ObjectID} = require('mongodb');
+
+const {toDo} = require('./../server/models/toDo');
+
+var id ='5cc170c7b9c9d4250883c32211';
+
+if(!ObjectID.isValid(id)){
+    return console.log('Invalid id');
+}
+
+/*toDo.find({
+    _id: id
+}).then((todos)=>{
+    console.log('todos by id',todos);
+});
+
+
+toDo.findOne({
+    _id: id
+}).then((todo)=>{
+    console.log('single todo by id',todo);
+});*/
+
+
+toDo.findById(id).then((todo)=>{
+    if(!todo){
+       return  console.log('Id not found');
+    }
+    console.log('todos by findById',todo);
+});
