554efbd54ea44b3457c04fa5859af03c18d072ff
unknown <engr.ab20@gmail.com>
2019-05-23 16:03:31 +0500

    configure database url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(   encodeURI('mongodb://admin:admin123@ds157256.mlab.com:57256/node-todos-api') ||'mongodb://localhost:27017/to_dos_app', {useNewUrlParser: true});
+//for heroku mlab add_ons
+// mongoose.connect(   process.env.MONGODB_URI ||'mongodb://localhost:27017/to_dos_app', {useNewUrlParser: true});
+
+//if using mlab direct link
+mongoose.connect(   encodeURI('mongodb://admin:admin123@ds157256.mlab.com:57256/node-todos-api'), {useNewUrlParser: true});
+
+//for local database, without internet
+// mongoose.connect('mongodb://localhost:27017/to_dos_app', {useNewUrlParser: true});
+
 //mongodb+srv://admin:<password>@node-todo-api-l4qzk.mongodb.net/test?retryWrites=true
 //encodeURI('mongodb+srv://admin:Public-1-2-3@node-todo-api-l4qzk.mongodb.net/test?retryWrites=true')
 module.exports={
