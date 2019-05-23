13c17a8d733c0c165fd4d07dd0197956db5453e7
unknown <engr.ab20@gmail.com>
2019-05-23 16:18:10 +0500

    updating moongoose.js file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -11,8 +11,8 @@ mongoose.connect(   encodeURI('mongodb://admin:admin123@ds157256.mlab.com:57256/
 //for local database, without internet
 // mongoose.connect('mongodb://localhost:27017/to_dos_app', {useNewUrlParser: true});
 
-//mongodb+srv://admin:<password>@node-todo-api-l4qzk.mongodb.net/test?retryWrites=true
 //encodeURI('mongodb+srv://admin:Public-1-2-3@node-todo-api-l4qzk.mongodb.net/test?retryWrites=true')
+
 module.exports={
     mongoose
 }
